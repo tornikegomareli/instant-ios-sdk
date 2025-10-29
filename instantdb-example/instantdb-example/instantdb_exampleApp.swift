142ca3547a77706ed93ffc0c4420ce17adf6c657
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-10-30 02:25:49 +0400

    Configure Clerk SDK in demo app
    - Adds Clerk SDK initialization with publishable key
    - Loads Clerk environment on app start
    - Configures test environment for development

--- instantdb-example/instantdb-example/instantdb_exampleApp.swift
+++ instantdb-example/instantdb-example/instantdb_exampleApp.swift
@@ -1,10 +1,15 @@
 import SwiftUI
 import InstantDB
 import GoogleSignIn
+import Clerk
 
 @main
 struct instantdb_exampleApp: App {
   let db = InstantClient(appID: "a8a567cc-34a7-41b4-8802-d81186ad7014")
+
+  init() {
+    Clerk.shared.configure(publishableKey: "pk_test_cHJpbWFyeS1jaGltcC02My5jbGVyay5hY2NvdW50cy5kZXYk")
+  }
 
   var body: some Scene {
     WindowGroup {
@@ -22,6 +27,9 @@ struct instantdb_exampleApp: App {
       .instantClient(db)
       .onOpenURL { url in
         GIDSignIn.sharedInstance.handle(url)
+      }
+      .task {
+        try? await Clerk.shared.load()
       }
     }
   }
