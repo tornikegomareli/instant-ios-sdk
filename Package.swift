9475b8accec8137a45607c437a46f195b62c5922
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-11-21 00:23:56 +0400

    feat: add swiftsyntaxmacros

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 6.2.0
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "InstantDB",
@@ -16,20 +17,43 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "9.0.0")
+        .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "9.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0")
     ],
     targets: [
+        // Macro implementation (compiler plugin)
+        .macro(
+            name: "InstantDBMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+
+        // Main library
         .target(
             name: "InstantDB",
             dependencies: [
+                "InstantDBMacros",
                 .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS")
             ],
             path: "Sources/InstantDB"
         ),
+
+        // Main tests
         .testTarget(
             name: "InstantDBTests",
             dependencies: ["InstantDB"],
             path: "Tests/InstantDBTests"
         ),
+
+        // Macro tests
+        .testTarget(
+            name: "InstantDBMacrosTests",
+            dependencies: [
+                "InstantDBMacros",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")
+            ]
+        )
     ]
 )
