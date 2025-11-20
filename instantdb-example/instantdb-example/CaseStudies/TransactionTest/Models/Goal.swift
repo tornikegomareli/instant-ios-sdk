a6e97649b58e98b7b4683eb6801d901fb1f94774
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-11-21 00:23:29 +0400

    feat: update models

--- instantdb-example/instantdb-example/CaseStudies/TransactionTest/Models/Goal.swift
+++ instantdb-example/instantdb-example/CaseStudies/TransactionTest/Models/Goal.swift
@@ -1,21 +1,12 @@
 import Foundation
 import InstantDB
 
-struct Goal: InstantEntity, Identifiable {
-  static var namespace: String { "goals" }
-
+@InstantEntity("goals")
+struct Goal {
   let id: String
   var title: String
   var difficulty: Int?
   var completed: Bool?
 
   var todos: [Todo]?
-
-  enum CodingKeys: String, CodingKey {
-    case id
-    case title
-    case difficulty
-    case completed
-    case todos
-  }
 }
--- instantdb-example/instantdb-example/CaseStudies/TransactionTest/Models/Todo.swift
+++ instantdb-example/instantdb-example/CaseStudies/TransactionTest/Models/Todo.swift
@@ -1,16 +1,9 @@
 import Foundation
 import InstantDB
 
-struct Todo: InstantEntity, Identifiable {
-    static var namespace: String { "todos" }
-
-    let id: String
-    var text: String
-    var done: Bool?
-
-    enum CodingKeys: String, CodingKey {
-        case id
-        case text
-        case done
-    }
+@InstantEntity("todos")
+struct Todo {
+  let id: String
+  var text: String
+  var done: Bool?
 }
