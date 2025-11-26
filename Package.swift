6340653ebd7fc3e4dcfa5538d2f6d29bf7fabee0
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-11-27 02:41:50 +0400

    add concurrency extras

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "9.0.0"),
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.3.2")
     ],
     targets: [
         // Macro implementation (compiler plugin)
@@ -35,7 +36,8 @@ let package = Package(
             name: "InstantDB",
             dependencies: [
                 "InstantDBMacros",
-                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS")
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras")
             ],
             path: "Sources/InstantDB"
         ),
